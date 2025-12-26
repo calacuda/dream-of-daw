a02d5275e163a34b363801b2fa8c5e1d85592fe7
Eoghan West <eowest@gmail.com>
2025-12-26 07:51:58 -0500

    outlined step button display

---
+++ dream-of-daw/dream_of_daw/step_buttons.py
@@ -0,0 +1,43 @@
+import pygame
+from .config import *
+
+
+button_bounds_w = STEP_BUTTON_BOUNDING_BOX.width / 8
+button_bounds_h = STEP_BUTTON_BOUNDING_BOX.height / 2
+button_w = button_bounds_w * 0.75
+button_h = button_bounds_h * 0.75
+
+
+def draw_a_button(i, button_grid_x, button_grid_y, selected: bool, playing: bool):
+    center_x = button_bounds_w * button_grid_x + \
+        button_bounds_w / 2 + STEP_BUTTON_BOUNDING_BOX.left
+    center_y = button_bounds_h * button_grid_y + \
+        button_bounds_h / 2 + STEP_BUTTON_BOUNDING_BOX.top
+
+    if selected:
+        outline_color = GREEN
+    else:
+        outline_color = SURFACE_2
+
+    if playing:
+        led_color = RED
+    else:
+        led_color = SURFACE_2
+
+    # TODO: add a text display for the playing note
+
+    button = pygame.Rect(center_x, center_y, button_w, button_h)
+    button.center = (center_x, center_y)
+    pygame.draw.rect(screen, outline_color, button,
+                     BUTTON_BOARDER_W, border_radius=5)
+    y = button.bottom - BUTTON_BOARDER_W * 4
+    pygame.draw.line(screen, led_color, (button.left + BUTTON_BOARDER_W *
+                     3.5, y), (button.right - BUTTON_BOARDER_W * 3.5, y), 5)
+
+
+def draw_steps_buttons():
+    for i in range(16):
+        button_grid_x = i % 8
+        button_grid_y = i // 8
+
+        draw_a_button(i, button_grid_x, button_grid_y, False, False)
